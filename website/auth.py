8fd2f7e2e929abd0c1a657404ec598d1c0ce2096
lycanzy <lycanzy@gmail.com>
2025-07-17 18:17:10 +0800

    added html template

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>Login Page</p>"
+    return render_template('login.html')
 
 @auth.route('/logout')
 def logout():
-    return "<p>Logout Page</p>"
+    return render_template('logout.html')
 
 @auth.route('/register')
 def register():
-    return "<p>Register Page</p>"
+    return render_template('register.html')
